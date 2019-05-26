926cd131f1372f629f08bd7117eacbdd451ad4a3
Axel Clark <axelclark2@yahoo.com>
2019-05-25 22:24:50 -0400

    Complete initial version of project

--- whos_at_the_door/lib/whos_at_the_door.ex
+++ whos_at_the_door/lib/whos_at_the_door.ex
@@ -1,18 +1,92 @@
 defmodule WhosAtTheDoor do
-  @moduledoc """
-  Documentation for WhosAtTheDoor.
-  """
+  @moduledoc false
+  use GenServer
+  require Logger
 
-  @doc """
-  Hello world.
+  defstruct ultrasonic: nil, greeting: false, visits: 0
 
-  ## Examples
+  alias GrovePi.{RGBLCD, Ultrasonic}
 
-      iex> WhosAtTheDoor.hello()
-      :world
+  @five_seconds 5000
 
-  """
-  def hello do
-    :world
+  ## Client API
+
+  def start_link(pin) do
+    GenServer.start_link(__MODULE__, pin)
+  end
+
+  ## Callbacks
+
+  @impl true
+  def init(ultrasonic_pin) do
+    state = %__MODULE__{ultrasonic: ultrasonic_pin}
+    {:ok, state, {:continue, :start_and_subscribe}}
+  end
+
+  @impl true
+  def handle_continue(:start_and_subscribe, state) do
+    Ultrasonic.subscribe(state.ultrasonic, :changed)
+    RGBLCD.initialize()
+    waiting_message()
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({_pin, :changed, %{value: value}}, %{greeting: false} = state)
+      when value < 100 do
+    new_state =
+      state
+      |> Map.replace!(:greeting, true)
+      |> Map.replace!(:visits, state.visits + 1)
+
+    hello_message(new_state)
+    log_distance(value)
+    set_greeting_reset()
+
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_info({_pin, :changed, %{value: value}}, state) do
+    log_distance(value)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info(:reset_greeting, state) do
+    waiting_message()
+    {:noreply, %{state | greeting: false}}
+  end
+
+  @impl true
+  def handle_info(_message, state) do
+    {:noreply, state}
+  end
+
+  ## Helpers
+
+  defp hello_message(state) do
+    RGBLCD.set_text("Hello! You are")
+    RGBLCD.set_cursor(1, 0)
+    RGBLCD.write_text("visitor #" <> Integer.to_string(state.visits))
+  end
+
+  defp log_distance(value) do
+    distance = ultrasonic_distance(value)
+    Logger.info(distance)
+  end
+
+  defp set_greeting_reset() do
+    Process.send_after(self(), :reset_greeting, @five_seconds)
+  end
+
+  defp ultrasonic_distance(value) when is_integer(value) do
+    Integer.to_string(value) <> " cm away"
+  end
+
+  defp waiting_message() do
+    RGBLCD.set_text("Waiting for a")
+    RGBLCD.set_cursor(1, 0)
+    RGBLCD.write_text("visitor!")
   end
 end
--- whos_at_the_door/lib/whos_at_the_door/application.ex
+++ whos_at_the_door/lib/whos_at_the_door/application.ex
@@ -3,13 +3,20 @@ defmodule WhosAtTheDoor.Application do
   # for more information on OTP Applications
   @moduledoc false
 
+  # Use port 3 for the ultrasonic
+  @ultrasonic_pin 3
+  # poll every 1 second
+  @ultrasonic_poll_interval 1_00
+
   use Application
+  import Supervisor.Spec, warn: false
 
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
       # Starts a worker by calling: WhosAtTheDoor.Worker.start_link(arg)
-      # {WhosAtTheDoor.Worker, arg}
+      worker(GrovePi.Ultrasonic, [@ultrasonic_pin, [poll_interval: @ultrasonic_poll_interval]]),
+      {WhosAtTheDoor, @ultrasonic_pin}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
